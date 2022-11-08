c14ff2c9641212724beac5b130efa78137d5d1f6
odessabear <bearodessa@gmail.com>
2022-11-08 03:00:31 +0200

    Swagger Tariff API implementation in the progress

--- homework_3-6/src/main/java/com/epam/spring/webprovider/controller/TariffController.java
+++ homework_3-6/src/main/java/com/epam/spring/webprovider/controller/TariffController.java
@@ -1,55 +1,42 @@
 package com.epam.spring.webprovider.controller;
 
+import com.epam.spring.webprovider.api.TariffApi;
 import com.epam.spring.webprovider.controller.dto.TariffDTO;
-import com.epam.spring.webprovider.controller.dto.group.OnCreate;
-import com.epam.spring.webprovider.controller.dto.group.OnUpdate;
 import com.epam.spring.webprovider.service.TariffService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/tariffs")
 @RequiredArgsConstructor
 @Slf4j
-public class TariffController {
+public class TariffController implements TariffApi {
     private final TariffService tariffService;
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping
-    public List<TariffDTO> getTariffsByCategoryName(@RequestParam(required = false) String categoryName) {
+    public List<TariffDTO> getTariffsByCategoryName(String categoryName) {
         log.info("getTariffs by categoryName (optional) {}", categoryName);
         return tariffService.getTariffsByCategoryName(categoryName);
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{id}")
-    public TariffDTO getTariffById(@PathVariable Integer id) {
+    public TariffDTO getTariffById(@PathVariable int id) {
         log.info("getTariff by id {}", id);
         return tariffService.getTariffById(id);
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public TariffDTO createTariff(@RequestBody @Validated(OnCreate.class) TariffDTO tariffDTO) {
+    public TariffDTO createTariff(TariffDTO tariffDTO) {
         log.info("creatTariff with name {}", tariffDTO.getName());
         return tariffService.createTariff(tariffDTO);
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/{id}")
-    public TariffDTO updateTariff(@PathVariable Integer id, @RequestBody @Validated(OnUpdate.class) TariffDTO tariffDTO) {
+    public TariffDTO updateTariff(int id, TariffDTO tariffDTO) {
         log.info("updateTariff with id {}", id);
         return tariffService.updateTariff(id, tariffDTO);
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    public void deleteTariff(@PathVariable Integer id) {
+    public void deleteTariff(int id) {
         log.info("deleteTariff with id {}", id);
         tariffService.deleteTariff(id);
     }
